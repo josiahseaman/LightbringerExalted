96bd964bbdcc93e6164d38c364b6bde1dbe406e9
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-03-17 23:37:28 -0600

    Working on Button Array appearance.

--- Charms/forms.py
+++ Charms/forms.py
@@ -5,6 +5,12 @@ from Charms.models import Charm
 
 class Button_Array(Select):
     template_name = 'floppyforms/button_array.html'
+    attrs = {0: "btn btn-default",
+             1: "btn btn-primary",
+             2: "btn btn-success",
+             3: "btn btn-info",
+             -1: "btn btn-warning",
+             -2: "btn btn-danger"}
 
 
 class CharmForm(ModelForm):
@@ -18,6 +24,7 @@ class CharmForm(ModelForm):
             'dice_bonus': Button_Array(),
             'ally_buff': Button_Array(),
             'negation': Button_Array(),
+            'speed_boost': Button_Array(),
             'unnatural_mental_influence': Button_Array(),
             'extra_willpower_to_resist': Button_Array(),
             'weakness': Button_Array(),
