97c4bc90fb3f1316031d171a249712d518b5f642
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-03-17 19:50:39 -0600

    Added an 'edit' view and url

--- Charms/urls.py
+++ Charms/urls.py
@@ -1,5 +1,7 @@
 __author__ = 'Josiah'
 from django.conf.urls import patterns, url  # do not delete this
 
-urlpatterns = patterns('', url(r'^Charm/new/$', 'Charms.views.new_charm'),
+urlpatterns = patterns('',
+                       url(r'^Charm/new/$', 'Charms.views.new_charm'),
+                       url(r'^Charm/(?P<primary_key>\d+)/$', 'Charms.views.edit_charm'),
                        )
--- Charms/views.py
+++ Charms/views.py
@@ -1,7 +1,8 @@
 from django.core.serializers import json
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from Charms.forms import CharmForm
+from Charms.models import Charm
 
 
 def save_new_instance(initialized_form, request):
@@ -10,7 +11,7 @@ def save_new_instance(initialized_form, request):
     if request.is_ajax():
         msg = {'pk': model_instance.pk, 'title': str(model_instance), 'model': model_title, 'status': 'success'}
         return HttpResponse(json.dumps(msg), content_type="application/json")
-    return redirect('/setup/%s/%i/' % (model_title, model_instance.pk))  # redirect to edit URL
+    return redirect('/Charm/%s/%i/' % (model_title, model_instance.pk))  # redirect to edit URL
 
 
 def new_form(request, initialized_form, context):
@@ -21,5 +22,19 @@ def new_form(request, initialized_form, context):
 
 def new_charm(request):
     initialized_form = CharmForm(request.POST or None)
-    context = {'title': 'Describe a Charm with Traits'}
+    context = {'form': initialized_form,
+               'title': 'Describe a Charm with Traits'}
     return new_form(request, initialized_form, context)
+
+
+def edit_charm(request, primary_key):
+    model = get_object_or_404(Charm, pk=primary_key)
+    initialized_form = CharmForm(request.POST or None, instance=model)
+    if initialized_form.is_valid() and request.method == 'POST':
+        initialized_form.save()  # write instance updates to database
+    context = {'form': initialized_form,
+               'title': "Edit an Existing Charm"}
+    return render(request, 'Charms/new.html', context)
+
+
+
