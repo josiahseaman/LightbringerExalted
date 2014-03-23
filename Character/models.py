e148d4d20b6ec9fa3594dfc29323d9ff117f50df
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-03-23 14:39:29 -0600

    Explicitly declared all stat fields.  Passing the first round of tests.

--- Character/models.py
+++ Character/models.py
@@ -1,25 +1,57 @@
 from django.db import models
 from Character.parsers import AnathemaParser
 
+def statField():
+    return models.IntegerField(default=0)
 
 class LightbringerCharacter(models.Model):
     _source_character_sheet = models.CharField(max_length=255)
-
-    @classmethod
-    def add_fields_by_dictionary(cls, instance, characterDict):
-        for key in characterDict:  # Add dict as fields
-            v = characterDict[key]
-            if isinstance(v, int):
-                field = models.IntegerField(default=characterDict[key])
-            elif isinstance(v, str):
-                field = models.CharField(max_length=255, default=characterDict[key])
-            elif isinstance(v, (dict, list)):
-                field = None  # TODO: handle dict and list
-            else:
-                raise ValueError("Unrecognized type in character dictionary %s: %s" % (str(key), str(v)))
-                # field = models.TextField(blank=True, null=True)
-            if field is not None:
-                cls.add_to_class(str(key).lower(), field)
+    name = models.CharField(max_length=255, default="Unnamed")  # May need to be extended for Deathknights
+    # Attributes
+    strength = statField()
+    dexterity = statField()
+    dex = statField()
+    stamina = statField()
+    charisma = statField()
+    manipulation = statField()
+    appearance = statField()
+    perception = statField()
+    intelligence = statField()
+    wits = statField()
+    # Abilities
+    archery = statField()
+    martialarts = statField()
+    melee = statField()
+    thrown = statField()
+    war = statField()
+    integrity = statField()
+    performance = statField()
+    presence = statField()
+    resistance = statField()
+    survival = statField()
+    craft = statField()
+    investigation = statField()
+    lore = statField()
+    medicine = statField()
+    occult = statField()
+    athletics = statField()
+    awareness = statField()
+    dodge = statField()
+    larceny = statField()
+    stealth = statField()
+    bureaucracy = statField()
+    linguistics = statField()
+    ride = statField()
+    sail = statField()
+    socialize = statField()
+    # Other stats
+    willpower = statField()
+    essence = statField()
+    limit = statField()
+    compassion = statField()
+    conviction = statField()
+    temperance = statField()
+    valor = statField()
 
     @classmethod
     def create(cls, _source_filename=None):
@@ -27,13 +59,37 @@ class LightbringerCharacter(models.Model):
         name = "Unnamed"
         if '.ecg' in _source_filename:
             parser = AnathemaParser(_source_filename)
-            characterSheet = parser.parse_to_dictionary()
+            characterDict = parser.parse_to_dictionary()
             # c.name = characterSheet['Name']
         else:
             print("I don't know how to parse this file.")
             c.characterSheet = {}
-
-        cls.add_fields_by_dictionary(c, characterSheet)
-
+        c = c.populate_fields(characterDict)
         return c
 
+    def populate_fields(self, characterDict):
+        for key in characterDict:
+            try:
+                self._meta.fields[key.lower()] = characterDict[key]
+            except:
+                print("No match found for: ", key)
+        return self
+
+    def getStat(self, statName):
+        return self.__dict__[statName.lower()]
+        # return self._meta.fields[statName.lower()]
+
+    def __getitem__(self, item):
+        return self.getStat(item)
+
+    def sumDicePoolWithoutPenalties(self, *stats):
+        dicePool = 0
+        for stat in stats: #I can do this with reduce, but it's harder to read
+            try:
+                dicePool += self[stat]
+            except AttributeError:
+                dicePool += stat #this is probably a number
+        return dicePool
+
+    def sumDicePool(self, *stats):
+        return self.sumDicePoolWithoutPenalties(*stats)
