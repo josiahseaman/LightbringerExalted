c5d74189b4fe8a5cc9877eb89241c4bd3fdd460e
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-03-22 22:13:42 -0600

    Created new App for Characters and all their associated models and forms.

---
+++ Character/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'Josiah'
+from django.conf.urls import patterns, url  # do not delete this
+
+urlpatterns = patterns('',
+                       url(r'^Character/new/$', 'Character.views.new_chararacter'),
+                       url(r'^Character/(?P<primary_key>\d+)/$', 'Chararacter.views.edit_character'),
+                       # TODO: Edit by character name (repo name)
+                       )
