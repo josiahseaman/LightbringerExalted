cc71d46e5df4788ca4a722bfb6807c61f5bee116
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-03-29 15:45:32 -0600

    Active trait pretty print available.

--- Charms/models.py
+++ Charms/models.py
@@ -17,7 +17,7 @@ class Charm(models.Model):
     scope = models.TextField()
     scope_power = models.IntegerField(default=0, choices=((-1,'Trick'), (0, 'Specialty'), (1, 'Ability'), (2, 'Caste Abilities')))
     duration = models.IntegerField(default=0, choices=choiceList('Instant', '5 activations', 'Scene or Dramatic Action', 'Permanent'))
-    magnitude = models.IntegerField(default=0, choices=choiceList('Single', '5 targets', 'Magnitude 3', 'Nation', 'Direcion'))
+    magnitude = models.IntegerField(default=0, choices=choiceList('Single', '5 targets', 'Magnitude 3', 'Nation', 'Direction'))
     dice_bonus = models.IntegerField(default=0, choices=choiceList('None', 'Augment (+3 dice)', 'Success Doubler', 'Perfect'))
     negation = models.IntegerField(default=0, choices=choiceList('None', 'Negate Penalties', 'Negate Requirement', 'Negate Normal Defense'))
     negation_detail = models.CharField(max_length=255, blank=True, null=True)
@@ -30,18 +30,35 @@ class Charm(models.Model):
                                                     db_column='additional_willpower_purchases')
     #other traits
     weakness = models.IntegerField(default=0, choices=((0, 'None'),
-                                                       (-1, 'Charm is easy to render Ineffective or Hard to Invoke  (Easily Overlooked, Immanent Glory)'),
+                                                       (-1, 'Charm is easy to render Ineffective or Hard to Invoke  (Easily Overlooked, Counter Attacks)'),
                                                        (-2, 'Charm constrains the characters Available Actions (Bloodthirsty Sword Dancer)')))
     narrative_benefit = models.IntegerField(default=0, choices=choiceList(0, 1, 2),
                                             help_text="modifiers that don't fit within the usual system (Ghost-Eating)")
     ally_buff = models.IntegerField(default=0, choices=choiceList('Self', 'Ally gets Charm Benefits', 'Gain Supernatural Powers'))
     counterattack = models.IntegerField(default=0, choices=((0, 'Normal'), (1, '+1 success per attack received'), (2, 'Counterattack')),
-                                        help_text='Enables a DV 0 attack on the attacker when your turn comes up')
+                                        help_text='Enables a DV 0 attack on the attacker when your turn comes up')#TODO: delete this field, add keyword
     #Keywords
     keywords = models.CharField(max_length=255, default='', blank=True, null=True,
                                 choices=doubleChoices('Crippling', 'Form-type', 'Holy', 'Knockback',
                                                        'Obvious', 'Peircing', 'Poison', 'Shaping', 'Sickness',
                                                         'Surprise', 'Touch', 'Training'))
-    #  'Stackable', only had 6 uses in the core book. 1 was unclear, 2 were unnecessary and the remainder could be modeled by 5x targets
 
+    def __str__(self):
+        return "%s: %s.  %s" % (self.name, self.scope, self.pretty_active_traits())
 
+    def active_traits(self):
+        traits_used = {}
+        for field_name in ['duration', 'magnitude', 'dice_bonus', 'negation', 'speed_boost',
+                           'unnatural_mental_influence', 'narrative_benefit', 'ally_buff', 'keywords']:
+            value = self.__dict__[field_name]
+            if value:  #This trait is used
+                # label = self._meta.fields[field_name].choices
+                traits_used[field_name] = value
+        return traits_used
+
+    def pretty_active_traits(self):
+        d = self.active_traits()
+        representations = []
+        for k, v in d.items():
+            representations.append("%s: %s" %(k, v))
+        return ", ".join(representations)
